71fa08693994eaacba30a66cb5bab73bb3b793d6
cxumol <cxumol@users.noreply.github.com>
2022-07-28 04:00:01 -0700

    Update data.py

--- data.py
+++ data.py
@@ -1,31 +1,36 @@
 url_list = [
             # "https://www.overcomingbias.com/archives", #temp
             "https://www.greaterwrong.com/?sort=hot",
-            "https://pattern.swarma.org/learn_paths",
-            "https://forum.obsidian.md/c/knowledge-management/6",
-            "https://rizime.substack.com/archive?sort=new",
+            "https://www.greaterwrong.com/?sort=new",
             "https://www.lesswrong.com/allPosts",
+            "https://www.alignmentforum.org/",
+            "https://forum.effectivealtruism.org/",
+            "https://www.lesswrong.com/",
+            "https://forum.effectivealtruism.org/allPosts",
+            "https://forum.obsidian.md/c/knowledge-management/6",
+            ##
+            "https://pattern.swarma.org/articles",
+            "https://pattern.swarma.org/learn_paths",
+            "https://pattern.swarma.org/wechat_articles",
+            "https://swarma.org/",
             "https://shimo.im/docs/tjRWrtPYp8JTrpHY/read", # no longer publicly accessible # resumed
             # "https://acacess.substack.com/archive?sort=new",
             "https://letters.acacess.com/page/2/",
-            "https://swarma.org/",
-            "https://www.alignmentforum.org/",
+            "https://rizime.substack.com/archive?sort=new",
             "http://headsalon.org/feed", # 
             "https://www.foretold.io/c/home/activity",
             "https://www.overcomingbias.com/",
             "https://intelligence.org/all-posts/",
             # "https://www.allnow.com/",
             # "https://www.weixinso.com/home/index/profile/bid/3894463207.html", # no update
-            "https://forum.effectivealtruism.org/allPosts",
+
             "https://bookdown.org/home/archive/",
-            "https://pattern.swarma.org/wechat_articles",
             # "https://sniggle.net/TPL/index5.php?entry=annotated",
             "https://sinceriously.fyi/", #
             "https://deconstructingyourself.com/all-articles", #
             "https://slatestarscratchpad.tumblr.com/archive", 
             "https://conge.github.io/archive/",
             "https://reader.one/",            
-            "https://www.greaterwrong.com/?sort=new",
             "https://astralcodexten.substack.com/archive?sort=new",
             # "http://openmindclub.blog.caixin.com/", # No longer updated
             "https://theangelphilosopher.com/",
@@ -34,16 +39,14 @@ url_list = [
             "https://daimajia.com/following",
             "https://maoruimas.github.io/zxcs/",
             "http://headsalon.org/catalog",
-            "https://forum.effectivealtruism.org/",
-            "https://www.lesswrong.com/",
             "https://diff.blog/",
-            "https://www.alignmentforum.org/",
             "https://revisesociology.com/",
             "https://www.metafilter.com",
-            "https://pattern.swarma.org/articles",
             "https://theanarchistlibrary.org/special/index",
             "https://zoomquiet.substack.com/",
             "https://slimemoldtimemold.com/",
+            "https://onehu.xyz/categories",
+            "https://deathisbadblog.com/",
             # "https://from-ai-to-zombies.eu/files.html", #temp
             # "https://www.readthesequences.com/Contents",#temp
             # "https://liqi.io/posts/", #temp
